061ccdeb3124f57a3cc9ff0f711ee7a8bc7a1ccc
tkc49 <h.tkc49@gmail.com>
2017-09-17 20:09:24 +0900

    fix bug

--- form-data-to-kintone-multiple-app.php
+++ form-data-to-kintone-multiple-app.php
@@ -70,7 +70,7 @@ class KintoneFormMultipleApp {
 		);
 
 		if ( is_admin() ) {
-		    new BFIGitHubPluginUpdater( __FILE__, 'tkc49', "form-data-to-kintone-attachments" );
+		    new BFIGitHubPluginUpdater( __FILE__, 'tkc49', "form-data-to-kintone-multiole-app" );
 		}		
 
 
